80c30eda539ff8a704782315adf281650dd47d90
Thiago2104 <santijipin@gmail.com>
2024-08-01 15:53:10 -0500

    Add service worker

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
+  build: {
+    emptyOutDir: false, // So that popup and content build files don't get deleted
+        target:"node16",
+        rollupOptions:{
+        input:{
+            background: "src/background/background.ts", // Entry Point
+            contentScript: 'src/content/contentScript.ts',
+        },
+        output:{
+            entryFileNames: "assets/[name].js"
+        }
+        },
+  }
 })
